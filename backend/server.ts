6f0693f11084a56d8ea6005fcf2efb5bd338a6a2
Jerome Steve Sahayadarlin <jsahayad@masonlive.gmu.edu>
2021-06-30 21:10:46 -0400

    adding initial account routes

--- backend/server.ts
+++ backend/server.ts
@@ -1,29 +1,17 @@
-// @ts-nocheck
-import { AlpacaClient, AlpacaStream } from '@master-chief/alpaca';
+import express from "express";
 
-const express = require("express");
+import { showAllAccountsRouter } from "./routes/account/accounts-all";
+import { showAccountTradingById } from "./routes/account/account-trading-id";
+import { showAccountById } from "./routes/account/account-by-id";
 
 // Configure env vars in env file
 require('dotenv').config();
 
-// init app
 const app = express();
 
-const client = new AlpacaClient({
-  credentials: {
-    key: process.env.APCA_API_KEY_ID,
-    secret: process.env.APCA_API_SECRET_KEY,
-    paper: true,
-  },
-  rate_limit: true,
-})
-
-const account = client.getAccount()
-console.log(account);
-
-// routes 
-// TODO: move these routes to separate files
-
+app.use(showAllAccountsRouter)
+app.use(showAccountById);
+app.use(showAccountTradingById);
 
 app.listen(3000, () => {
   console.log('Listening on 3000');
