f6adcc757886e2f8739a99801c6fda7f7132d00c
Jerome Steve Sahayadarlin <jsahayad@masonlive.gmu.edu>
2021-07-17 13:42:43 -0400

    finished auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,30 @@
-import express from 'express';
-import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import { app }  from './app';
 
-const app = express();
-app.use(json());
+const start = async () => {
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-})
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+};
+
+start();
